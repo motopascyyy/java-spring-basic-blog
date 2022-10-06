38f40406d99fa52935399e0f1b6b196690d930f3
motopascyyy <pasdeignan@gmail.com>
2022-10-06 10:31:21 -0700

    Module check

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -51,6 +51,10 @@ public class PostRepository {
     }
 
     public Post findById(Long id) {
+        for (Post p : ALL_POSTS) {
+            if (p.getId().equals(id))
+                return p;
+        }
         return null;
     }
 }
